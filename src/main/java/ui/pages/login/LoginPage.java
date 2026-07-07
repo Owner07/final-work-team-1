571b3934b5516467b1d8f32c4d768f213a6e3e96
Veit Vladimir <makoveev20122012@gmail.com>
2026-07-08 00:25:02 +0500

    grooming struct project

--- src/main/java/ui/pages/login/LoginPage.java
+++ src/main/java/ui/pages/login/LoginPage.java
@@ -2,6 +2,7 @@ package ui.pages.login;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 import ui.pages.base.BasePage;
@@ -22,6 +23,7 @@ public class LoginPage extends BasePage {
         return authorizationTitle;
     }
 
+    @Step("Open login page")
     public LoginPage open() {
         log.info("Opening login page");
         Selenide.open("/");
@@ -29,6 +31,7 @@ public class LoginPage extends BasePage {
         return this;
     }
 
+    @Step("Login with user: {user} and password: ****")
     public LoginPage login(String user, String password) {
         // Проверяем, что страница логина открыта
         if (!isPageOpened()) {
